ef74abd85c1f0cba89e941526a8adaca2b1a322b
Youssef Mostafa <y.ismail@tekunda.com>
2025-07-16 18:11:19 +0300

    fix/missing field added

--- backend/app/hotel/hotel.py
+++ backend/app/hotel/hotel.py
@@ -33,6 +33,7 @@ class Hotel(MongoBaseModel): # Beanie Document model for MongoDB the base class
     tax_number: str   # Integrated under HotelAdmin, can be optional
     review_count: int = 0 # Number of reviews, default to 0
     rating: Optional[float] = None # Average rating, can be None if no reviews exist
+    hotel_admin_id: Optional[PydanticObjectId] = None  # ID of the hotel admin who manages this hotel
 
 
 # Response model that includes the serialized ID
