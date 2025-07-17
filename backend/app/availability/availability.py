a8e56c37c43b033f8ab9b9a57f83951ad179fdea
Youssef Mostafa <y.ismail@tekunda.com>
2025-07-17 13:10:33 +0300

    FIXING FIELDS FOR THE SCHEMA

--- backend/app/availability/availability.py
+++ backend/app/availability/availability.py
@@ -5,8 +5,8 @@ from bson import ObjectId
 from common_models.common import MongoBaseModel, PydanticObjectId
 
 class Availability(MongoBaseModel):
-    hotel_id: Optional[str] = None  # ID of the hotel for which availability is being checked
-    room_id: str  # ID of the room for which availability is being checked
+    hotel_id: Optional[PydanticObjectId] = None  # ID of the hotel for which availability is being checked
+    room_id:  Optional[PydanticObjectId] = None # ID of the room for which availability is being checked
     date: str  # Date in "YYYY-MM-DD" format
     total_rooms: int  # Total number of rooms for the specified date and room
     reserved_rooms: int  # Number of rooms already reserved for the specified date and room
--- backend/app/reservation/reservation.py
+++ backend/app/reservation/reservation.py
@@ -10,9 +10,9 @@ class Status(Enum):
     PENDING = "pending"
 
 class Reservation(MongoBaseModel):
-    hotel_id: Optional[str]=None  # ID of the hotel being reserved
-    user_id:  Optional[str]=None # ID of the user making the reservation
-    room_id:  Optional[str]=None # ID of the room being reserved
+    hotel_id:  Optional[PydanticObjectId] = None  # ID of the hotel being reserved
+    user_id:   Optional[PydanticObjectId] = None # ID of the user making the reservation
+    room_id:   Optional[PydanticObjectId] = None # ID of the room being reserved
     start_date: str  # Start date of the reservation in "YYYY-MM-DD" format
     end_date: str  # End date of the reservation in "YYYY-MM-DD" format
     status: Status  # Status of the reservation (e.g., "confirmed", "cancelled")
